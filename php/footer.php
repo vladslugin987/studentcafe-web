1db8ff53f60f537314e893e1b245ac6632a38b93
liaacharlotte <lia@liacharlotte.de>
2026-01-11 22:18:17 +0100

    tabindex und form labels

--- php/footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo '<p>WS 2025/2026, Grundlagen der Web-Technologien</p> -
-    <p>Daniil Bavin, Lia Duppel, Sarah Hofmann, Vladislav Slugin</p>
-    <a href="impressum.html">Impressum</a> 
-    <a href="datenschutz.html">Datenschutzerklärung</a>';
-
-echo phpversion();
