bbc9c2d329913ee94042ccadf7e9faa0404ce205
liaacharlotte <lia@liacharlotte.de>
2026-01-12 18:03:30 +0100

    update form style and js

--- js/formulare.js
+++ js/formulare.js
@@ -1,7 +1,30 @@
-function check(self) {
-            if (self.textfeld.value == "") {
-                alert("Bitte Daten eingeben");
-                return false;
-            }
-            return true;
-        }
+// function check(self) {
+//     if (self.textfeld.value == "") {
+//         alert("Bitte Daten eingeben");
+//         return false;
+//     }
+//     return true;
+// };
+
+function validateForm() {
+    let name = document.forms["myForm"]["fname"].value;
+    let mail = document.forms["myForm"]["mail"].value;
+    let betreff = document.forms["myForm"]["betreff"].value;
+    let nachricht = document.forms["myForm"]["tipp"].value;
+    if (name == "" || mail == "" || betreff == "" || nachricht == "") {
+        alert("Bitte gebe deine Daten ein");
+        return false;
+    }
+};
+
+function validateFormReservieren() {
+    let name = document.forms["myForm"]["name"].value;
+    let personen = document.forms["myForm"]["personen"].value;
+    let date = document.forms["myForm"]["date"].value;
+    let mail = document.forms["myForm"]["mail"].value;
+    let phone = document.forms["myForm"]["phone"].value;
+    if (name == "" || personen == "" || date == "" || mail == "" || phone == "") {
+        alert("Bitte gebe deine Daten ein");
+        return false;
+    }
+};
